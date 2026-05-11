fc3cf1f04b70b4102f19cb78748696194232646d
mutuRSSA <kmkp.rssa@gmail.com>
2026-05-11 18:55:17 +0700

    Integrate WA notifications

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ const API_CONFIG = {
     // Ganti URL di bawah ini dengan URL Deployment Apps Script Anda yang terbaru
     AUTH: "https://script.google.com/macros/s/AKfycbwnQ4Fjx98BJSR2SNt3PwwYgqcjx6R6HtBIfPscWQFuuW1c1QagJYJD9Q9p04fJRdyk5w/exec",
     MUTU: "https://script.google.com/macros/s/AKfycbwRvJx8dx2Ncd66q6fVAVpVNeE4wqN9omcDuVQKupfmjtJ6UU5D4_t68j-dGgj1Wyky/exec",
-    IKP: "https://script.google.com/macros/s/AKfycbwpBwmFGTKTKOElHOtK3C_ISOhfKxnFgePqhKu3b2beAT7Qxwnyr8BER54BG3-J3qiJ/exec",
-    KPC: "https://script.google.com/macros/s/AKfycbxuu_u_pYsmfSYAoy9arI76xSrGddgSSQMsBzjknysotJF2b0Rk5n7v25AjAe6LbBpi/exec",
+    IKP: "https://script.google.com/macros/s/AKfycbyIBs80-GnO7bmG_h4c0E7i90NbjtZPRMDv-KzyJFvj2_hKyXpAyYeqiLeQISkVXbzS/exec",
+    KPC: "https://script.google.com/macros/s/AKfycbzGfqCkTJop4zuoHJAFxOyEuoQLczpj1ult9B296czfaiweKhJdMjI75KTK4YooYyUt/exec",
     SURVEY: "https://script.google.com/macros/s/AKfycbzs7-x6GGX8dMqLgYOXZfNDY3127xbSDEnibba5yM0YcXWiShBWo9Yh1yS9JQrsoEU2/exec",
-    RISK: "https://script.google.com/macros/s/AKfycbw5AIpb23MiMTHlJ57zipzdEyCsR7wMVDgk9MMN687UQ1IkdyyfIUlJP5u4tayGftxm8g/exec",
+    RISK: "https://script.google.com/macros/s/AKfycbxxHcHIwmqoNehlW30V7jEpxD5MgR75KW_Hl5DA9sZPkOQXecw8AyqrxmEoJQKTv7oDpg/exec",
     PROFIL_RS: "https://script.google.com/macros/s/AKfycbzGMUFIYrzzLMkWwx7fysIX9xFaM4IvBT7sedc70nn-t8pMFHal0HYv2G6jF05QT6l2lA/exec",
     // Tambahkan URL menu dinamis dari components.js
     MENU: "https://script.google.com/macros/s/AKfycbxRcpdb3tnHFWXWVLBzIymqAQnmygkxc_QoRVR43At859Yi6ZwYNkN0mSJaaKa5i4GJ/exec" 
