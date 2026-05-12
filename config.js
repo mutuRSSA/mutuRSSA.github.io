21bd9b77df095a62c5c135786b80fef0b6cbfd80
mutuRSSA <kmkp.rssa@gmail.com>
2026-05-12 08:44:54 +0700

    Fix IKP KPC Backend

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const API_CONFIG = {
     // Ganti URL di bawah ini dengan URL Deployment Apps Script Anda yang terbaru
     AUTH: "https://script.google.com/macros/s/AKfycbwnQ4Fjx98BJSR2SNt3PwwYgqcjx6R6HtBIfPscWQFuuW1c1QagJYJD9Q9p04fJRdyk5w/exec",
     MUTU: "https://script.google.com/macros/s/AKfycbwRvJx8dx2Ncd66q6fVAVpVNeE4wqN9omcDuVQKupfmjtJ6UU5D4_t68j-dGgj1Wyky/exec",
-    IKP: "https://script.google.com/macros/s/AKfycbyIBs80-GnO7bmG_h4c0E7i90NbjtZPRMDv-KzyJFvj2_hKyXpAyYeqiLeQISkVXbzS/exec",
-    KPC: "https://script.google.com/macros/s/AKfycbzGfqCkTJop4zuoHJAFxOyEuoQLczpj1ult9B296czfaiweKhJdMjI75KTK4YooYyUt/exec",
+    IKP: "https://script.google.com/macros/s/AKfycby1E0lMbT_xPRyJvNFS2PqsLA_eKcEettPZRN0bpdvXsRjf2Ya8lt8jpo2oZ40_gyut/exec",
+    KPC: "https://script.google.com/macros/s/AKfycbxXgbToxjU7n_IAvLjJe27atbWfG2cNOXRgNSnCNyMXdALGdM8V3wVNXG3dwPjsFkF9/exec",
     SURVEY: "https://script.google.com/macros/s/AKfycbzs7-x6GGX8dMqLgYOXZfNDY3127xbSDEnibba5yM0YcXWiShBWo9Yh1yS9JQrsoEU2/exec",
     RISK: "https://script.google.com/macros/s/AKfycbxxHcHIwmqoNehlW30V7jEpxD5MgR75KW_Hl5DA9sZPkOQXecw8AyqrxmEoJQKTv7oDpg/exec",
     PROFIL_RS: "https://script.google.com/macros/s/AKfycbzGMUFIYrzzLMkWwx7fysIX9xFaM4IvBT7sedc70nn-t8pMFHal0HYv2G6jF05QT6l2lA/exec",
