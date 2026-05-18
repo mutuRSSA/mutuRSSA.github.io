8233784ea00035af5a36602a264be23bbc28ffa2
mutuRSSA <kmkp.rssa@gmail.com>
2026-05-18 15:08:42 +0700

    change profil indikator page

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const API_CONFIG = {
     // Ganti URL di bawah ini dengan URL Deployment Apps Script Anda yang terbaru
     AUTH: "https://script.google.com/macros/s/AKfycbwnQ4Fjx98BJSR2SNt3PwwYgqcjx6R6HtBIfPscWQFuuW1c1QagJYJD9Q9p04fJRdyk5w/exec",
-    MUTU: "https://script.google.com/macros/s/AKfycbyEXjWuOCbcT4aklB2ZKVo4HET6Q807pP_-ABYHm-2d1iikV1vD2t3XhwdnQdsIdfUR/exec",
+    MUTU: "https://script.google.com/macros/s/AKfycbzVrZ9fQdZvJacNHsa5vJDFUmrg6ODdj-dG2BwlLPs99EAl7l4UFqR355yxGRpNOk9w/exec",
     IKP: "https://script.google.com/macros/s/AKfycby1E0lMbT_xPRyJvNFS2PqsLA_eKcEettPZRN0bpdvXsRjf2Ya8lt8jpo2oZ40_gyut/exec",
     KPC: "https://script.google.com/macros/s/AKfycbxXgbToxjU7n_IAvLjJe27atbWfG2cNOXRgNSnCNyMXdALGdM8V3wVNXG3dwPjsFkF9/exec",
     SURVEY: "https://script.google.com/macros/s/AKfycbzs7-x6GGX8dMqLgYOXZfNDY3127xbSDEnibba5yM0YcXWiShBWo9Yh1yS9JQrsoEU2/exec",
