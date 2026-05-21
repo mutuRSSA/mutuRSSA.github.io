e4360b8458bace37ca03ff9376e3488e7d8c73d1
mutuRSSA <kmkp.rssa@gmail.com>
2026-05-21 15:35:46 +0700

    Add FMEA

--- config.js
+++ config.js
@@ -9,8 +9,7 @@ const API_CONFIG = {
     IKP: "https://script.google.com/macros/s/AKfycby1E0lMbT_xPRyJvNFS2PqsLA_eKcEettPZRN0bpdvXsRjf2Ya8lt8jpo2oZ40_gyut/exec",
     KPC: "https://script.google.com/macros/s/AKfycbxXgbToxjU7n_IAvLjJe27atbWfG2cNOXRgNSnCNyMXdALGdM8V3wVNXG3dwPjsFkF9/exec",
     SURVEY: "https://script.google.com/macros/s/AKfycbzs7-x6GGX8dMqLgYOXZfNDY3127xbSDEnibba5yM0YcXWiShBWo9Yh1yS9JQrsoEU2/exec",
-    RISK: "https://script.google.com/macros/s/AKfycbx2Vb3KYhvkGJEkLiYgQm3q0cXGsyd9rTXFvt-3Y3uBbs71t8h6rRScbCwU_T6xx5dygA/exec",
-    PROFIL_RS: "https://script.google.com/macros/s/AKfycbzGMUFIYrzzLMkWwx7fysIX9xFaM4IvBT7sedc70nn-t8pMFHal0HYv2G6jF05QT6l2lA/exec",
+    RISK: "https://script.google.com/macros/s/AKfycbwvfIp7CKHIubVRYwfdDwOB3pynLWBP5hUsc4Q9OlAMAVVaRyHoQs0PB7UqNL4njtwJ/exec",
     // Tambahkan URL menu dinamis dari components.js
     MENU: "https://script.google.com/macros/s/AKfycbxRcpdb3tnHFWXWVLBzIymqAQnmygkxc_QoRVR43At859Yi6ZwYNkN0mSJaaKa5i4GJ/exec" 
 };
