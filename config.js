0d4ba3066ac9d6aeb2f67a616e986b431f03cb23
mutuRSSA <kmkp.rssa@gmail.com>
2026-05-31 12:09:52 +0700

    Fix analisis

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const API_CONFIG = {
     // Ganti URL di bawah ini dengan URL Deployment Apps Script Anda yang terbaru
     AUTH: "https://script.google.com/macros/s/AKfycbwnQ4Fjx98BJSR2SNt3PwwYgqcjx6R6HtBIfPscWQFuuW1c1QagJYJD9Q9p04fJRdyk5w/exec",
     MUTU: "https://script.google.com/macros/s/AKfycbzVrZ9fQdZvJacNHsa5vJDFUmrg6ODdj-dG2BwlLPs99EAl7l4UFqR355yxGRpNOk9w/exec",
-    IKP: "https://script.google.com/macros/s/AKfycby1E0lMbT_xPRyJvNFS2PqsLA_eKcEettPZRN0bpdvXsRjf2Ya8lt8jpo2oZ40_gyut/exec",
+    IKP: "https://script.google.com/macros/s/AKfycbyPJJlzfUfHyU87IRhetFiEgU83VM-84OYa5AH31DHFFV1JKn_opYjPkzwBlu8nQKVo/exec",
     KPC: "https://script.google.com/macros/s/AKfycbxXgbToxjU7n_IAvLjJe27atbWfG2cNOXRgNSnCNyMXdALGdM8V3wVNXG3dwPjsFkF9/exec",
     SURVEY: "https://script.google.com/macros/s/AKfycbzs7-x6GGX8dMqLgYOXZfNDY3127xbSDEnibba5yM0YcXWiShBWo9Yh1yS9JQrsoEU2/exec",
     RISK: "https://script.google.com/macros/s/AKfycbzLjKpvIVhnd0k4PcX6AyQDQ_sE-LQi16UsuPKgtiDKdI-KDpJMN1_IsvgubvPAC3QSZA/exec",
