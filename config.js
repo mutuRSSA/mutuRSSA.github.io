2b9e48c9f9b27f7a98a6e5165716b16a832f1f43
mutuRSSA <kmkp.rssa@gmail.com>
2026-06-02 20:35:30 +0700

    add automatic rekap

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const API_CONFIG = {
     // Ganti URL di bawah ini dengan URL Deployment Apps Script Anda yang terbaru
     AUTH: "https://script.google.com/macros/s/AKfycbwnQ4Fjx98BJSR2SNt3PwwYgqcjx6R6HtBIfPscWQFuuW1c1QagJYJD9Q9p04fJRdyk5w/exec",
-    MUTU: "https://script.google.com/macros/s/AKfycbzVrZ9fQdZvJacNHsa5vJDFUmrg6ODdj-dG2BwlLPs99EAl7l4UFqR355yxGRpNOk9w/exec",
+    MUTU: "https://script.google.com/macros/s/AKfycbwh_8rRAEjsvt3Gb5PFg60I8nhu0L76_44ikv6OhhSFIW8xpItWXrBQlNZvP0dujK0F/exec",
     IKP: "https://script.google.com/macros/s/AKfycbyPJJlzfUfHyU87IRhetFiEgU83VM-84OYa5AH31DHFFV1JKn_opYjPkzwBlu8nQKVo/exec",
     KPC: "https://script.google.com/macros/s/AKfycbxXgbToxjU7n_IAvLjJe27atbWfG2cNOXRgNSnCNyMXdALGdM8V3wVNXG3dwPjsFkF9/exec",
     SURVEY: "https://script.google.com/macros/s/AKfycbzs7-x6GGX8dMqLgYOXZfNDY3127xbSDEnibba5yM0YcXWiShBWo9Yh1yS9JQrsoEU2/exec",
