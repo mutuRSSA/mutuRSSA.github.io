00c0740f27580474b9f2731e70fc6747395d6a89
mutuRSSA <kmkp.rssa@gmail.com>
2026-06-03 20:14:51 +0700

    Fix FMEA

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const API_CONFIG = {
     IKP: "https://script.google.com/macros/s/AKfycbyPJJlzfUfHyU87IRhetFiEgU83VM-84OYa5AH31DHFFV1JKn_opYjPkzwBlu8nQKVo/exec",
     KPC: "https://script.google.com/macros/s/AKfycbxXgbToxjU7n_IAvLjJe27atbWfG2cNOXRgNSnCNyMXdALGdM8V3wVNXG3dwPjsFkF9/exec",
     SURVEY: "https://script.google.com/macros/s/AKfycbzs7-x6GGX8dMqLgYOXZfNDY3127xbSDEnibba5yM0YcXWiShBWo9Yh1yS9JQrsoEU2/exec",
-    RISK: "https://script.google.com/macros/s/AKfycbzLjKpvIVhnd0k4PcX6AyQDQ_sE-LQi16UsuPKgtiDKdI-KDpJMN1_IsvgubvPAC3QSZA/exec",
+    RISK: "https://script.google.com/macros/s/AKfycbztzI8Gu1NcLPJgw0xy6lf4mqnmqy31V0FKDcj9ipPy9m84lEJ2-_jSTCzvUjOfud6g9g/exec",
     // Tambahkan URL menu dinamis dari components.js
     MENU: "https://script.google.com/macros/s/AKfycbxRcpdb3tnHFWXWVLBzIymqAQnmygkxc_QoRVR43At859Yi6ZwYNkN0mSJaaKa5i4GJ/exec" 
 };
