143d5b8dedd752af6b756503f49e568995d4bbb2
mutuRSSA <kmkp.rssa@gmail.com>
2026-06-30 09:52:24 +0700

    fix profil

--- config.js
+++ config.js
@@ -5,13 +5,13 @@
 const API_CONFIG = {
     // Ganti URL di bawah ini dengan URL Deployment Apps Script Anda yang terbaru
     AUTH: "https://script.google.com/macros/s/AKfycbwnQ4Fjx98BJSR2SNt3PwwYgqcjx6R6HtBIfPscWQFuuW1c1QagJYJD9Q9p04fJRdyk5w/exec",
-    MUTU: "https://script.google.com/macros/s/AKfycbwgbQVnaD2ZJvZadlZW9_pZOE_2lLVYsw0rOj2v7xI2GVgokt_ypqI8WxfcIbwj4EI/exec",
+    MUTU: "https://script.google.com/macros/s/AKfycbw3yLlqk2SP0s2FXPJiLNi-I9-y-OQlYszeGqWwRyb3xr5gOXHghjRGxK3jeGc9vEpA/exec",
     IKP: "https://script.google.com/macros/s/AKfycbyPJJlzfUfHyU87IRhetFiEgU83VM-84OYa5AH31DHFFV1JKn_opYjPkzwBlu8nQKVo/exec",
     KPC: "https://script.google.com/macros/s/AKfycbxXgbToxjU7n_IAvLjJe27atbWfG2cNOXRgNSnCNyMXdALGdM8V3wVNXG3dwPjsFkF9/exec",
     SURVEY: "https://script.google.com/macros/s/AKfycbzs7-x6GGX8dMqLgYOXZfNDY3127xbSDEnibba5yM0YcXWiShBWo9Yh1yS9JQrsoEU2/exec",
     RISK: "https://script.google.com/macros/s/AKfycbztzI8Gu1NcLPJgw0xy6lf4mqnmqy31V0FKDcj9ipPy9m84lEJ2-_jSTCzvUjOfud6g9g/exec",
     // Tambahkan URL menu dinamis dari components.js
     MENU: "https://script.google.com/macros/s/AKfycbxRcpdb3tnHFWXWVLBzIymqAQnmygkxc_QoRVR43At859Yi6ZwYNkN0mSJaaKa5i4GJ/exec",
-    COMPILER: "https://script.google.com/macros/s/AKfycbz7q_f8gp0d2UU7Rmaw-fhbBPcZw1yCQCHF_-E2UUAx6sIXjWRkGvtW67Bt4GsL3nSc/exec",
+    COMPILER: "https://script.google.com/macros/s/AKfycbxB7waKrAYgNWCNCXHZcG46eD5i8NkcoRVnULvEb9nRp79rlBXLNXKi9HB5n5Cp8OBd/exec",
     PPI: "https://script.google.com/macros/s/AKfycbw1J8m83-DeYBGpb4mB8-pI1SotM-xSzwbSGlDhUaA0DUGxu2AZTf6BfsYydGPL4XI9/exec"
 };
